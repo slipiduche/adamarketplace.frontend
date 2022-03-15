0acff18b110d3505aac1d9669d359b315a43047a
slipiduche <alejandrocamacaro91@gmail.com>
2022-03-15 15:34:43 +0000

    deleting unused files

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const apiKey = process.env.REACT_APP_BLOCKFROST_API_KEY;
 export const cardanoUrl = process.env.REACT_APP_BLOCKFROST_API_URL;
 
 export const urls = {
-  root: "http://martify.io/",
+  root: "http://ec2-52-207-243-145.compute-1.amazonaws.com:3000/",
   cardanoscan: process.env.REACT_APP_CARDANOSCAN_URL,
   ipfs: "https://infura-ipfs.io/ipfs/"
 }
--- src/database/assets.js
+++ src/database/assets.js
@@ -11,37 +11,37 @@ import {
   where,
 } from "firebase/firestore";
 import { getAssetDetails, getMintedAssets } from "../cardano/blockfrost-api";
-import { firestore } from "../firebase";
 
-/**
- * @throws COULD_NOT_SAVE_ASSET_TO_DB
- */
-export const addAssetEvent = async (asset, newEvent) => {
-  if (asset && newEvent) {
-    const assetUpdated = {
-      ...asset,
-      events: [...(asset.events || []), newEvent],
-    };
-    await saveAsset(assetUpdated);
-    return assetUpdated;
-  }
-  return asset;
-};
 
-/**
- * @throws COULD_NOT_SAVE_ASSET_TO_DB
- */
-export const addAssetOffer = async (asset, newOffer) => {
-  if (asset && newOffer) {
-    const assetUpdated = {
-      ...asset,
-      offers: [...(asset.offers || []), newOffer],
-    };
-    await saveAsset(assetUpdated);
-    return assetUpdated;
-  }
-  return asset;
-};
+// /**
+//  * @throws COULD_NOT_SAVE_ASSET_TO_DB
+//  */
+// export const addAssetEvent = async (asset, newEvent) => {
+//   if (asset && newEvent) {
+//     const assetUpdated = {
+//       ...asset,
+//       events: [...(asset.events || []), newEvent],
+//     };
+//     await saveAsset(assetUpdated);
+//     return assetUpdated;
+//   }
+//   return asset;
+// };
+
+// /**
+//  * @throws COULD_NOT_SAVE_ASSET_TO_DB
+//  */
+// export const addAssetOffer = async (asset, newOffer) => {
+//   if (asset && newOffer) {
+//     const assetUpdated = {
+//       ...asset,
+//       offers: [...(asset.offers || []), newOffer],
+//     };
+//     await saveAsset(assetUpdated);
+//     return assetUpdated;
+//   }
+//   return asset;
+// };
 
 /**
  * @throws COULD_NOT_RETRIEVE_ASSET_FROM_DB
@@ -93,146 +93,146 @@ export const getAssets = async (assetIds) => {
 /**
  * @throws COULD_NOT_RETRIEVE_COLLECTION_ASSETS_FROM_DB
  */
-export const getCollectionAssets = async (
-  policyId,
-  page = 1,
-  count = 100,
-  lastVisible
-) => {
-  try {
-    if (policyId) {
-      const reference = await query(
-        collection(firestore, "assets"),
-        where("details.policyId", "==", policyId),
-        orderBy("details.readableAssetName"),
-        startAfter(lastVisible?.details?.readableAssetName ?? ""),
-        limit(count)
-      );
+// export const getCollectionAssets = async (
+//   policyId,
+//   page = 1,
+//   count = 100,
+//   lastVisible
+// ) => {
+//   try {
+//     if (policyId) {
+//       const reference = await query(
+//         collection(firestore, "assets"),
+//         where("details.policyId", "==", policyId),
+//         orderBy("details.readableAssetName"),
+//         startAfter(lastVisible?.details?.readableAssetName ?? ""),
+//         limit(count)
+//       );
 
-      const snapshot = await getDocs(reference);
+//       const snapshot = await getDocs(reference);
 
-      if (snapshot.empty || snapshot.docs.length < count) {
-        const assetIds = await getMintedAssets(policyId, { page, count });
-        return await getAssets(assetIds);
-      } else {
-        return snapshot.docs.map((doc) => doc.data());
-      }
-    }
-    return [];
-  } catch (error) {
-    console.error(
-      `Unexpected error in getCollectionAssets. [Message: ${error.message}]`
-    );
-    throw new Error("COULD_NOT_RETRIEVE_COLLECTION_ASSETS_FROM_DB");
-  }
-};
+//       if (snapshot.empty || snapshot.docs.length < count) {
+//         const assetIds = await getMintedAssets(policyId, { page, count });
+//         return await getAssets(assetIds);
+//       } else {
+//         return snapshot.docs.map((doc) => doc.data());
+//       }
+//     }
+//     return [];
+//   } catch (error) {
+//     console.error(
+//       `Unexpected error in getCollectionAssets. [Message: ${error.message}]`
+//     );
+//     throw new Error("COULD_NOT_RETRIEVE_COLLECTION_ASSETS_FROM_DB");
+//   }
+// };
 
-/**
- * @param {string} address - address needs to be in bech32 format.
- * @throws COULD_NOT_SAVE_ASSET_TO_DB
- */
-export const lockAsset = async (
-  asset,
-  { datum, datumHash, txHash, address, artistAddress, contractVersion }
-) => {
-  if (
-    asset &&
-    datum &&
-    datumHash &&
-    txHash &&
-    address &&
-    artistAddress &&
-    contractVersion
-  ) {
-    const assetUpdated = {
-      ...asset,
-      status: {
-        datum,
-        datumHash,
-        locked: true,
-        txHash,
-        artistAddress,
-        contractVersion,
-        submittedBy: address,
-        submittedOn: new Date().getTime(),
-      },
-    };
-    await saveAsset(assetUpdated);
-    return assetUpdated;
-  }
-  return asset;
-};
+// /**
+//  * @param {string} address - address needs to be in bech32 format.
+//  * @throws COULD_NOT_SAVE_ASSET_TO_DB
+//  */
+// export const lockAsset = async (
+//   asset,
+//   { datum, datumHash, txHash, address, artistAddress, contractVersion }
+// ) => {
+//   if (
+//     asset &&
+//     datum &&
+//     datumHash &&
+//     txHash &&
+//     address &&
+//     artistAddress &&
+//     contractVersion
+//   ) {
+//     const assetUpdated = {
+//       ...asset,
+//       status: {
+//         datum,
+//         datumHash,
+//         locked: true,
+//         txHash,
+//         artistAddress,
+//         contractVersion,
+//         submittedBy: address,
+//         submittedOn: new Date().getTime(),
+//       },
+//     };
+//     await saveAsset(assetUpdated);
+//     return assetUpdated;
+//   }
+//   return asset;
+// };
 
-/**
- * @param {string} address - address needs to be in bech32 format.
- * @throws COULD_NOT_SAVE_ASSET_TO_DB
- */
-export const unlockAsset = async (asset, { txHash, address }) => {
-  if (asset && txHash && address) {
-    const assetUpdated = {
-      ...asset,
-      status: {
-        locked: false,
-        txHash,
-        submittedBy: address,
-        submittedOn: new Date().getTime(),
-      },
-    };
-    await saveAsset(assetUpdated);
-    return assetUpdated;
-  }
-  return asset;
-};
+// /**
+//  * @param {string} address - address needs to be in bech32 format.
+//  * @throws COULD_NOT_SAVE_ASSET_TO_DB
+//  */
+// export const unlockAsset = async (asset, { txHash, address }) => {
+//   if (asset && txHash && address) {
+//     const assetUpdated = {
+//       ...asset,
+//       status: {
+//         locked: false,
+//         txHash,
+//         submittedBy: address,
+//         submittedOn: new Date().getTime(),
+//       },
+//     };
+//     await saveAsset(assetUpdated);
+//     return assetUpdated;
+//   }
+//   return asset;
+// };
 
-/**
- * @throws COULD_NOT_RETRIEVE_LOCKED_ASSETS_FROM_DB
- */
-export const getLockedAssets = async (count = 100, lastVisible) => {
-  try {
-    const reference = await query(
-      collection(firestore, "assets"),
-      where("status.locked", "==", true),
-      orderBy("status.submittedOn"),
-      startAfter(lastVisible?.status?.submittedOn ?? 0),
-      limit(count)
-    );
+// /**
+//  * @throws COULD_NOT_RETRIEVE_LOCKED_ASSETS_FROM_DB
+//  */
+// export const getLockedAssets = async (count = 100, lastVisible) => {
+//   try {
+//     const reference = await query(
+//       collection(firestore, "assets"),
+//       where("status.locked", "==", true),
+//       orderBy("status.submittedOn"),
+//       startAfter(lastVisible?.status?.submittedOn ?? 0),
+//       limit(count)
+//     );
 
-    const snapshot = await getDocs(reference);
+//     const snapshot = await getDocs(reference);
 
-    if (snapshot.empty) return [];
-    return snapshot.docs.map((doc) => doc.data());
-  } catch (error) {
-    console.error(
-      `Unexpected error in getLockedAssets. [Message: ${error.message}]`
-    );
-    throw new Error("COULD_NOT_RETRIEVE_LOCKED_ASSETS_FROM_DB");
-  }
-};
+//     if (snapshot.empty) return [];
+//     return snapshot.docs.map((doc) => doc.data());
+//   } catch (error) {
+//     console.error(
+//       `Unexpected error in getLockedAssets. [Message: ${error.message}]`
+//     );
+//     throw new Error("COULD_NOT_RETRIEVE_LOCKED_ASSETS_FROM_DB");
+//   }
+// };
 
-/**
- * @throws COULD_NOT_SAVE_ASSET_TO_DB
- */
-export const saveAsset = async (asset) => {
-  try {
-    if (asset) {
-      const reference = doc(firestore, "assets", asset.details.asset);
-      await setDoc(reference, asset, { merge: true });
-    }
-  } catch (error) {
-    console.error(`Unexpected error in saveAsset. [Message: ${error.message}]`);
-    throw new Error("COULD_NOT_SAVE_ASSET_TO_DB");
-  }
-};
+// /**
+//  * @throws COULD_NOT_SAVE_ASSET_TO_DB
+//  */
+// export const saveAsset = async (asset) => {
+//   try {
+//     if (asset) {
+//       const reference = doc(firestore, "assets", asset.details.asset);
+//       await setDoc(reference, asset, { merge: true });
+//     }
+//   } catch (error) {
+//     console.error(`Unexpected error in saveAsset. [Message: ${error.message}]`);
+//     throw new Error("COULD_NOT_SAVE_ASSET_TO_DB");
+//   }
+// };
 
-/**
- * @throws COULD_NOT_SAVE_ASSET_TO_DB
- */
-export const saveAssets = async (assets) => {
-  if (assets) {
-    await Promise.all(
-      assets.map(async (asset) => {
-        await saveAsset(asset);
-      })
-    );
-  }
-};
+// /**
+//  * @throws COULD_NOT_SAVE_ASSET_TO_DB
+//  */
+// export const saveAssets = async (assets) => {
+//   if (assets) {
+//     await Promise.all(
+//       assets.map(async (asset) => {
+//         await saveAsset(asset);
+//       })
+//     );
+//   }
+// };
